ee814e73c88759c9a7d5ef5573fb444d1b1cda5e
Forrest <rainforest92@126.com>
2020-06-05 11:42:41 +0800

    :sparkles: cros

--- akos.config.js
+++ akos.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     type: 'app',
     // host: '127.0.0.1',
-    port: 7000,
+    port: 80,
     root: __dirname,
     base: '/',
     srcPath: './server',
     entry: './server/app',
     routesPath: './server/routes.js',
     controllersPath: './server/controllers',
-    middlewares: ['requestLogger', 'IPLimit'],
+    middlewares: ['requestLogger', 'IPLimit', 'cors'],
 };
